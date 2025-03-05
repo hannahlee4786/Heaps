c6dcfec9ba1b11bd20acddb8a3b8af4e86395d2c
Hannah Lee <hlee4786@usc.edu>
2025-03-04 21:12:37 -0800

    stack class commit

--- stack.h
+++ stack.h
@@ -4,20 +4,60 @@
 #include <vector>
 #include <stdexcept>
 
-// Use inheritance from std::vector (choose public/private) as appropriate
+// Use private inheritance from std::vector
 template <typename T>
-class Stack 
+class Stack : private std::vector<T>
 {
 public:
     Stack();
     ~Stack();
+
     bool empty() const;
     size_t size() const;
     void push(const T& item);
-    void pop();  // throws std::underflow_error if empty
+    void pop(); // throws std::underflow_error if empty
     const T& top() const; // throws std::underflow_error if empty
-    // Add other members only if necessary
 };
 
+template <typename T>
+Stack<T>::Stack() {
+
+}
+
+template <typename T>
+Stack<T>::~Stack() {
+
+}
+
+template <typename T>
+bool Stack<T>::empty() const {
+    return std::vector<T>::empty();
+}
+
+template <typename T>
+size_t Stack<T>::size() const {
+    return std::vector<T>::size();
+}
+
+template <typename T>
+void Stack<T>::push(const T& item) {
+    this->push_back(item);
+}
+
+template <typename T>
+void Stack<T>::pop() {
+    if (this->empty()) {
+        throw std::underflow_error("Stack is empty.");
+    }
+    this->pop_back();
+}
+
+template <typename T>
+const T& Stack<T>::top() const {
+    if (this->empty()) {
+        throw std::underflow_error("Stack is empty.");
+    }
+    return this->back();
+}
 
 #endif
